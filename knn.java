8ce97ac79d0afe32531de7d3e7ab0226dd0dd42c
DersioK <805494@students.wits.ac.za>
2019-08-06 12:03:42 +0200

    Added x and y coordinate arrays

--- knn.java
+++ knn.java
@@ -7,13 +7,22 @@ public class knn{
             System.out.println();
             for(int j=0;j<matrix[i].length;j++){
                 matrix[i][j] =0;
-               // matrix[j][i] =0;
-               System.out.print(matrix[i][j]);
-              }
-         }
+                // matrix[j][i] =0;
+                System.out.print(matrix[i][j]);
+            }
+        }
         System.out.println();
         return null;
-        
+    }
+    //Still wokring on this
+    public static double distance(ArrayList <Integer> X, ArrayList <Integer> Y){
+        double  dist=0;
+        double z = 2;
+        for(int i =0;i<X.size();i++){
+            dist  = (int)(Math.sqrt((Math.pow(Y.get(i)-X.get(i),2))+Math.pow(Y.get(i+1)-X.get(i+1),2)));
+            System.out.println(dist);
+        }
+        return dist;
     }
     public static void main(String[] args){
         String s,g;
@@ -21,49 +30,41 @@ public class knn{
         String co_ord[];
         String co_ord2[];
         int k,p,n,d;
+        ArrayList <Integer> y_cords = new ArrayList<Integer>();
+        ArrayList <Integer> x_cords = new ArrayList<Integer>();
+        ArrayList <Integer> z = new ArrayList<Integer>();
         Scanner in = new Scanner(System.in);
         s = in.nextLine();
         spaces = s.split(" ");
-        
         int [] q =new int[spaces.length];
         for(int i=0;i<spaces.length;i++){
-          q[i]=Integer.parseInt(spaces[i]);
+            q[i]=Integer.parseInt(spaces[i]);
         }
         k = q[0];
         p = q[1];
         n = q[2];
         adjMatrix(q[3]);
-        
         Scanner e = new Scanner(System.in);
-        while(e.hasNextLine()){
+        while(e.hasNextLine()) {
             g = e.nextLine();
-        if(!(g.equals("Exit"))){
-        co_ord2= g.split(";");
-        co_ord = g.split(",");
-       
-        int [] z =new int[co_ord.length];
-        for(int i=0;i<co_ord.length;i++){
-          z[i]=Integer.parseInt(co_ord[i]);
+            if(!g.equalsIgnoreCase("end")) {
+                co_ord = g.split(",");
+                for (int i = 0; i < co_ord.length; i++) {
+                     z.add(Integer.parseInt(co_ord[i]));
+                }
+            }
+            else{break;}
         }
-
-        int [] x_cords =new int[z.length];
-        for(int i=0;i<z.length;i=i+2){
-           x_cords[i]=z[i];
-          // System.out.print("X coordinates: "+x_cords[i]);
+        for (int i = 0; i < z.size(); i = i + 2) {
+            x_cords.add(z.get(i));
         }
-        int [] y_cords =new int[z.length];
-        for(int i=1;i<z.length;i=i+2){
-            y_cords[i]=z[i];
-            
+        for (int i = 1; i < z.size(); i = i + 2) {
+            y_cords.add(z.get(i));
         }
-        for(int i=0;i<x_cords.length;i++){
-            System.out.print("X coordinates: "+x_cords[i]);
-        }
+        System.out.print("X: "+ x_cords);
         System.out.println();
-        for(int i=0;i<y_cords.length;i++){
-            System.out.print("Y coordinates: "+y_cords[i]);
+        System.out.print("Y "+y_cords);
+        System.out.println();
+        distance(x_cords,y_cords);
         }
     }
-  }
- }
-}
