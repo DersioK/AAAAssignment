7c8755d5456e435e3eb32e770973f6aaaf753a98
DersioK <805494@students.wits.ac.za>
2019-08-14 10:51:45 +0200

    Separated classes and modified knn.java to Main.java

---
+++ obstacle_points.java
@@ -0,0 +1,31 @@
+class obstacle_points{
+    public int x;
+    public int y;
+
+    public obstacle_points(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    public obstacle_points()
+    {
+
+    }
+    public double distance(int x,int y){
+        return x;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
